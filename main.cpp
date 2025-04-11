f38bd5a33f705b0ba4a82d4333de980a6b863926
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2025-04-11 13:47:41 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+
+int main()
+{
+    std::string s1 = "acbcf";
+    std::string s2 = "abcdaf";
+    int n = s1.length();
+    int m = s2.length();
+
+    std::cout << "LCS-Recursive of S1 and S2: "
+              << DynamicProgramming::LCS::Recursive(s1, s2, n, m)
+              << std::endl;
+
+    std::memset(t, -1, sizeof(t));
+    std::cout << "LCS-memoization of S1 and S2: "
+              << DynamicProgramming::LCS::memoization(s1, s2, n, m)
+              << std::endl;
+
+    std::cout << "LCS-Top Down of S1 and S2: "
+              << DynamicProgramming::LCS::Topdown(s1, s2, n, m)
+              << std::endl;
+
+    std::cout << "LongestCommSubstring: "
+              << DynamicProgramming::LCS::LongestCommSubstring(s1, s2, n, m)
+              << std::endl;
+
+    std::cout << "Printing Longest common subsequence: "
+              << DynamicProgramming::LCS::printLongestCommSubsequence(s1, s2, n, m)
+              << std::endl;
+    system("pause>0");
+    return 0;
+}
