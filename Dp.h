d91c76928f97e5b82a9301551dbd0bf06682e8c3
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2025-04-16 15:45:54 +0530

    Update Dp.h
    Matrix Chain Multiplication
    -recursion

--- Dp.h
+++ Dp.h
@@ -355,5 +355,25 @@ namespace DynamicProgramming
             return false;    
         }
     }
+    namespace MatrixChainMultiplication 
+    {
+        int Recursive(int arr[], int i, int j)
+        {
+            if(i>=j){
+                return 0;
+            }
+            int mn = INT_MAX;
+            for(int k=i; k<=j-1; k++){
+                long long tempAns = Recursive(arr, i, k) +
+                                    Recursive(arr,k+1, j)+
+                                    arr[i-1]*arr[k]*arr[j];
+
+                if(tempAns<mn){
+                    mn = tempAns;
+                }                    
+            }
+            return mn;
+        }
+    }
 
 }
